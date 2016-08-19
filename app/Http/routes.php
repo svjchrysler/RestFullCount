1308857f5540992817d105c15b6aef6f43093c4a
jcsalguero <jcsalguero>
2016-08-19 17:16:01 -0400

    cambios

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
 Route::post('person/store', 'PersonController@store');
 Route::post('car/store', 'CarController@store');
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,22 +8,63 @@
         <link rel="stylesheet" href="css/bootstrap.min.css">
     </head>
     <body>
-        <table>
-            <tr>
-                <td>gdfgfggfd</td>
-                <td>fgsdfsdf</td>
-                <td>fgsdfsdddfgsdfgf</td>
-            </tr>
-            @foreach($data as $dt)
-            <tr>
-                <td>{{ $dt->nombre_encuestador }}</td>
-                <td>{{ $dt->hora_incio }}</td>
-                <td>{{ $dt->fecha }}</td>
-            </tr>
-            @endforeach
-            
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <h3>Datos Estadisticos</h3>
+            </div>
+        </div>    
+    </div>
 
-        </table>
+    
+
+    <br><br>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <table class="table table-striped table-bordered">
+                    <tr>
+                        <td>Nombre</td>
+                        <td>Particular</td>
+                        <td>Bicicleta</td>
+                        <td>Motocicleta</td>
+                        <td>Taxi</td>
+                        <td>Publico</td>
+                        <td>Repartidor</td>
+                        <td>Calle Relevamiento</td>
+                        <td>Calle A</td>
+                        <td>Calle B</td>
+                        <td>Temperatura</td>
+                        <td>Condiciones</td>
+                        <td>Hora</td>
+                        <td>Fecha</td>
+                        <td>Nota</td>
+                    </tr>
+                    @foreach($data as $dt)
+                    <tr>
+                        <td>{{ $dt->nombre_encuestador }}</td>
+                        <td>{{ $dt->particular }}</td>
+                        <td>{{ $dt->bicicleta }}</td>
+                        <td>{{ $dt->motocicleta }}</td>
+                        <td>{{ $dt->taxi }}</td>
+                        <td>{{ $dt->publico }}</td>
+                        <td>{{ $dt->repartidor }}</td>
+                        <td>{{ $dt->calle_relevamiento }}</td>
+                        <td>{{ $dt->calle_lateral_a }}</td>
+                        <td>{{ $dt->calle_lateral_b }}</td>
+                        <td>{{ $dt->temperatura }}</td>
+                        <td>{{ $dt->condiciones }}</td>
+                        <td>{{ $dt->hora_inicio.' - '.$dt->hora_fin }}</td>
+                        <td>{{ $dt->fecha }}</td>
+                        <td>{{ $dt->nota }}</td>
+                    </tr>
+                    @endforeach
+                </table>
+            </div>
+        </div>
+    </div>
+    
+        
         
         <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
         <script src="js/bootstrap.min.js"></script>
